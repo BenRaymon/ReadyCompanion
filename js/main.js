aac88bb1beffa074ff5f10b6f3702561163520ae
BenRaymon <bendraymon@gmail.com>
2024-01-08 13:43:45 -0500

    Full size banner. Updated contact form and added JS logic for sending email

--- js/main.js
+++ js/main.js
@@ -25,3 +25,71 @@ $('.nav-link').on('click', (event) => {
     $('.nav-link').removeClass('active');
     event.target.classList.add('active');
 });
+
+
+/* Send Email - Contact Form */
+// TODO - set emailjs key
+// emailjs.init('');
+
+async function sendEmail(formElement) {
+    // TODO - set emailjs service and template keys
+    // let res = await emailjs.sendForm('service_KEY', 'template_KEY', formElement);
+    // console.log(res);
+}
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const formElement = $('#contactform')[0];
+    const formData = new FormData(formElement);
+
+    let valid = true;
+    for (const item of formData.entries()) {
+        valid = validateInputs(item[0], item[1]) ? valid : false;
+    }
+
+    if (valid) {
+        await sendEmail(formElement);
+        formElement.reset();
+    }
+}
+
+function validateInputs(key, value) {
+    let valid = true;
+    switch (key) {
+        case 'user_name':
+            valid = isNotWhiteSpace(value);
+            addValidation(key, valid);
+            break;
+        case 'user_email':
+            valid = isValidEmail(value);
+            addValidation(key, valid);
+            break;
+    }
+    return valid;
+}
+
+function isNotWhiteSpace(text) {
+    let hasEverything = /\S/.test(text) && text !== undefined && text !== null;
+    if (typeof text === "object") {
+        if (text.size === 0) {
+            hasEverything = false;
+        }
+    }
+    return hasEverything;
+}
+
+function isValidEmail(email) {
+    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    return regex.test(email);
+}
+
+function addValidation(fieldId, isValid) {
+    if (!isValid) {
+        $(`#${fieldId}`).addClass('is-invalid');
+    } else {
+        $(`#${fieldId}`).removeClass('is-invalid');
+    }
+}
+
+$('#submit-message').on('click', (event) => { handleFormSubmit(event); });
